4642a14c809082cfc7623b4d24663dda487d81dc
KangsanBae <warimo47@naver.com>
2020-07-28 17:43:09 +0900

    First commit

---
+++ NumberCounting/NumberCounting/NumberCounting.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2020 Kangsan Bae <warimo47@naver.com>. All rights reserved.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+	std::ifstream numberFile{ "01+=+[5천개]+45+적용+=+0001.use") };
+	std::string str = "";
+	
+	if (numberFile.is_open() == true)
+	{
+		while (numberFile.eof() == false)
+		{
+
+		}
+	}
+
+	
+
+	std::getline(numberFile, str);
+
+
+	std::cout << allText;
+
+	getchar();
+}
