ce35edcac53c9d394c45080773ac57c8b297cf9c
KangsanBae <warimo47@naver.com>
2021-03-05 11:43:11 +0900

    Add ChangeClasses.cpp Add MoveFolder.cpp

---
+++ FileCleanup/FileCleanup/ChangeClasses.cpp
@@ -0,0 +1,117 @@
+// ChangeClasses.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <io.h>
+
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem::v1;
+
+std::vector<std::string> get_files_inDirectory(const std::string&, const std::string&);
+
+int main()
+{
+    std::string oldFolder = "C:/Users/warim/Desktop/YOLO_TRAIN/Yolo_mark-master/x64/Release/data/backup/";
+    std::string newFolder = "C:/Users/warim/Desktop/YOLO_TRAIN/Yolo_mark-master/x64/Release/data/newimg/";
+
+    std::vector<std::string> fileNames = get_files_inDirectory(oldFolder, "*.txt");
+
+    std::ifstream txtFileReader;
+    std::ofstream txtFileWriter;
+
+    char oneRectangleLine[50];
+    std::string oneRectangleLineStr;
+
+    char* context = nullptr;
+
+    char* classNum;
+    std::string classNumStr;
+
+    char changeClassLine[50];
+
+    std::string writeLine;
+
+    for (auto fn : fileNames)
+    {
+        // std::cout << oldFolder + fn << "\n";
+        txtFileReader.open(oldFolder + fn, std::ios_base::in);
+        txtFileWriter.open(newFolder + fn, std::ios_base::out);
+
+        while (txtFileReader.peek() != EOF)
+        {
+            std::getline(txtFileReader, oneRectangleLineStr);
+
+            strcpy_s(oneRectangleLine, oneRectangleLineStr.c_str());
+            classNum = strtok_s(oneRectangleLine, " ", &context);
+            classNumStr = classNum;
+
+            // std::cout << classNumStr << "\n";
+            if (classNumStr == "1") // person
+            {
+                writeLine = "6" + oneRectangleLineStr.substr(1) + "\n"; // People
+                txtFileWriter.write(writeLine.c_str(), writeLine.length());
+            }
+            else if (classNumStr == "3") // car
+            {
+                writeLine = "4" + oneRectangleLineStr.substr(1) + "\n"; // Vehicle
+                txtFileWriter.write(writeLine.c_str(), writeLine.length());
+            }
+            else if (classNumStr == "6") // bus
+            {
+                writeLine = "4" + oneRectangleLineStr.substr(1) + "\n"; // Vehicle
+                txtFileWriter.write(writeLine.c_str(), writeLine.length());
+            }
+            else if (classNumStr == "8") // truck
+            {
+                writeLine = "11" + oneRectangleLineStr.substr(1) + "\n"; // Ex_truck
+                txtFileWriter.write(writeLine.c_str(), writeLine.length());
+            }
+            else // else
+            {
+
+            }
+        }
+
+        txtFileReader.close();
+        txtFileWriter.close();
+
+        // std::cout << fn << "\n";
+    };
+
+    std::cout << "Done.\n";
+}
+
+// find all matched file in specified directory
+// [INPUT]
+//   >> const std::string& _path   Search path   ex) c:/directory/
+//   >> const std::string& _filter Search filter ex) *.exe or *.*
+// [RETURN]
+//   >> std::vector<std::string>   All matched file name & extension
+std::vector<std::string> get_files_inDirectory(const std::string& _path, const std::string& _filter)
+{
+    std::string searching = _path + _filter;
+
+    std::vector<std::string> return_;
+
+    _finddata_t fd;
+    long handle = _findfirst(searching.c_str(), &fd);  // 현재 폴더 내 모든 파일을 찾는다.
+
+    if (handle == -1) return return_;
+
+    int result = 0;
+    do
+    {
+        return_.push_back(fd.name);
+        result = _findnext(handle, &fd);
+    } while (result != -1);
+
+    _findclose(handle);
+
+    return return_;
+}
---
+++ FileCleanup/FileCleanup/MoveFolder.cpp
@@ -0,0 +1,58 @@
+// ChangeClasses.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <io.h>
+
+#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+
+#include <experimental/filesystem>
+
+namespace fs = std::experimental::filesystem::v1;
+
+std::vector<std::string> get_files_inDirectory(const std::string&, const std::string&);
+
+int main()
+{
+    std::string oldFolder = "C:/Users/warim/Desktop/YOLO_TRAIN/Yolo_mark-master/x64/Release/data/newimg/";
+    std::string newFolder = "C:/Users/warim/Desktop/YOLO_TRAIN/Yolo_mark-master/x64/Release/data/backup/";
+
+    std::vector<std::string> fileNames = get_files_inDirectory(oldFolder, "*.txt");
+
+    for (auto fn : fileNames)
+    {
+        fs::rename(oldFolder + fn, newFolder + fn);
+    };
+}
+
+// find all matched file in specified directory
+// [INPUT]
+//   >> const std::string& _path   Search path   ex) c:/directory/
+//   >> const std::string& _filter Search filter ex) *.exe or *.*
+// [RETURN]
+//   >> std::vector<std::string>   All matched file name & extension
+std::vector<std::string> get_files_inDirectory(const std::string& _path, const std::string& _filter)
+{
+    std::string searching = _path + _filter;
+
+    std::vector<std::string> return_;
+
+    _finddata_t fd;
+    long handle = _findfirst(searching.c_str(), &fd);  // 현재 폴더 내 모든 파일을 찾는다.
+
+    if (handle == -1) return return_;
+
+    int result = 0;
+    do
+    {
+        return_.push_back(fd.name);
+        result = _findnext(handle, &fd);
+    } while (result != -1);
+
+    _findclose(handle);
+
+    return return_;
+}
