90ec0e37d5e293a0f20cc700d66c65ec0b23830d
KangsanBae <warimo47@naver.com>
2022-05-10 21:58:17 +0900

    Add Algorithm project

---
+++ Algorithm/Maze/Player.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "pch.h"
+
+class Board;
+
+class Player
+{
+	enum
+	{
+		MOVE_TICK = 100
+	};
+
+public:
+	void Init(Board* board);
+	void Update(uint64 deltaTick);
+
+	void SetPos(Pos pos) { _pos = pos; };
+	Pos GetPos() { return _pos; };
+
+	bool CanGo(Pos pos);
+
+private:
+	Pos _pos = {};
+	int32 _dir = DIR_UP;
+	Board* _board = nullptr;
+	vector<Pos> _path;
+	uint32 _pathIndex = 0;
+	uint64 _sumTick = 0;
+};
+
