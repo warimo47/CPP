be32ae041c1cd305a66819a9b5f6319d6b58a40c
KangsanBae <warimo47@gmail.com>
2022-05-24 16:50:36 +0900

    BinarySearchTree practice 4

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -34,17 +34,21 @@ int main()
 {
     BinarySearchTree bst;
 
-    bst.Insert(30);
     bst.Insert(20);
     bst.Insert(10);
-    bst.Insert(25);
     bst.Insert(26);
+    bst.Insert(25); 
     bst.Insert(40);
+    bst.Insert(30);
     bst.Insert(50);
 
     bst.PrintTree();
 
-    cout << endl;
+    cout << endl << endl;
+
+    bst.Delete(26);
+
+    bst.PrintTree();
 }
 
 void MyVectorCheck()
--- Algorithm/Algorithm/BinarySearchTree.cpp
+++ Algorithm/Algorithm/BinarySearchTree.cpp
@@ -26,6 +26,52 @@ void BinarySearchTree::PrintNode(int step, BSTNode* node)
 	PrintNode(step + 1, node->right);
 }
 
+BSTNode* BinarySearchTree::Min(BSTNode* node)
+{
+	while (node->left)
+		node = node->left;
+
+	return node;
+}
+
+BSTNode* BinarySearchTree::Max(BSTNode* node)
+{
+	while (node->right)
+		node = node->right;
+
+	return node;
+}
+
+BSTNode* BinarySearchTree::Next(BSTNode* node)
+{
+	if (node->right)
+		return Min(node->right);
+
+	BSTNode* parents = node->parents;
+
+	while (parents && node == parents->right)
+		parents = node->parents;
+
+	return parents;
+}
+
+BSTNode* BinarySearchTree::Search(int key)
+{
+	BSTNode* node = _root;
+
+	while (node)
+	{
+		if (node->key == key)
+			break;
+		else if (node->key < key)
+			node = node->right;
+		else
+			node = node->left;
+	}
+
+	return node;
+}
+
 void BinarySearchTree::Insert(int key)
 {
 	BSTNode* newNode = new BSTNode();
@@ -62,5 +108,44 @@ void BinarySearchTree::Insert(int key)
 
 void BinarySearchTree::Delete(int key)
 {
+	BSTNode* node = Search(key);
+	if (node)
+		Delete(node);
+	else
+		std::cout << "없는 노드입니다." << std::endl;
+}
 
+void BinarySearchTree::Delete(BSTNode* node)
+{
+	if (node->left == nullptr)
+		Replace(node, node->right);
+	else if (node->right == nullptr)
+		Replace(node, node->left);
+	else
+	{
+		BSTNode* next = Next(node);
+
+		node->key = next->key;
+
+		Delete(next);
+	}
+}
+
+void BinarySearchTree::Replace(BSTNode* b, BSTNode* a)
+{
+	if (b == nullptr)
+	{
+		std::cout << "대체하려는 대상 노드가 nullptr입니다." << std::endl;
+		return;
+	}
+
+	if (b == b->parents->right)
+		b->parents->right = a;
+	else
+		b->parents->left = a;
+
+	if (a)
+		a->parents = b->parents;
+
+	delete b;
 }
